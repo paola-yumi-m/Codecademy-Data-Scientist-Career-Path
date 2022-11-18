789fc265b28c903cdd9560bc888b9508d00bbec3
paola-yumi-m <paola.yumi.m@gmail.com>
2022-11-18 20:55:42 -0300

    testing filterByTerm

--- getting-started-with-jest/__tests__/filterByTerm.spec.js
+++ getting-started-with-jest/__tests__/filterByTerm.spec.js
@@ -1,6 +1,7 @@
 function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, 'i');
     return inputArr.filter(function(arrayElement) {
-        return arrayElement.url.match(searchTerm);
+        return arrayElement.url.match(regex);
     });
 }
 
@@ -17,5 +18,36 @@ describe('Filter function', () => {
         expect(filterByTerm(input, 'link')).toEqual(output);
 
         expect(filterByTerm(input, 'LINK')).toEqual(output);
-    })
+    });
+
+    test('should filter by search term (uRl)', () => {
+        const input = [
+            { id: 1, url: 'https://www.url1.dev' },
+            { id: 2, url: 'https://www.url2.dev' },
+            { id: 3, url: 'https://www.link3.dev' }
+        ];
+         
+        const output = [
+            { id: 1, url: 'https://www.url1.dev' },
+            { id: 2, url: 'https://www.url2.dev' }
+        ];
+
+        expect(filterByTerm(input, 'uRl')).toEqual(output);
+    });
+
+    test('should filter by search term (uRl)', () => {
+        const input = [
+            { id: 1, url: 'https://www.url1.dev' },
+            { id: 2, url: 'https://www.url2.dev' },
+            { id: 3, url: 'https://www.link3.dev' }
+        ];
+         
+        const output = [
+            { id: 1, url: 'https://www.url1.dev' },
+            { id: 2, url: 'https://www.url2.dev' },
+            { id: 3, url: 'https://www.link3.dev' }
+        ];
+
+        expect(filterByTerm(input, '')).toEqual(output);
+    });
 })
