5acd666ccbc2296593ed8648497f8215f3600768
paola-yumi-m <paola.yumi.m@gmail.com>
2022-11-21 20:13:55 -0300

    finishing tests

--- getting-started-with-jest/__tests__/filterByTerm.spec.js
+++ getting-started-with-jest/__tests__/filterByTerm.spec.js
@@ -1,9 +1,4 @@
-function filterByTerm(inputArr, searchTerm) {
-    const regex = new RegExp(searchTerm, 'i');
-    return inputArr.filter(function(arrayElement) {
-        return arrayElement.url.match(regex);
-    });
-}
+const filterByTerm = require('../src/filterByTerm');
 
 describe('Filter function', () => {
     test('it should filter by a search term (link)', () => {
@@ -35,19 +30,15 @@ describe('Filter function', () => {
         expect(filterByTerm(input, 'uRl')).toEqual(output);
     });
 
-    test('should filter by search term (uRl)', () => {
+    test('should throw error when searchTerm is empty string ("")', () => {
         const input = [
             { id: 1, url: 'https://www.url1.dev' },
             { id: 2, url: 'https://www.url2.dev' },
             { id: 3, url: 'https://www.link3.dev' }
         ];
-         
-        const output = [
-            { id: 1, url: 'https://www.url1.dev' },
-            { id: 2, url: 'https://www.url2.dev' },
-            { id: 3, url: 'https://www.link3.dev' }
-        ];
 
-        expect(filterByTerm(input, '')).toEqual(output);
+        expect(() => 
+        {filterByTerm(input, '')
+        }).toThrow("searchTerm cannot be empty");
     });
 })
